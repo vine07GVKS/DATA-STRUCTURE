cfd9d4579df8f740c981efb65cfb74b5d6dfb94a
vine07GVKS <vinethjawa2001@gmail.com>
2023-12-17 10:37:12 +0530

    Abstrac_class completed

--- java ds salem/data_structure/src/main/java/java_practice_code/Abstrac_class.java
+++ java ds salem/data_structure/src/main/java/java_practice_code/Abstrac_class.java
@@ -2,10 +2,18 @@ package java_practice_code;
 
 public class Abstrac_class {
     public static void main(String[] args) {
-
+     HP obj = new HP();
+     obj.turnOn();
     }
 }
 
 abstract class computer{
+        void  turnOn(){
+            System.out.println("Turning ON ");
+        }
+}
+
+class HP extends computer{
+
 
 }
